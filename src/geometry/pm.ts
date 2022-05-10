4034621cd539d0b29f474d6fb4f55b941b2e6e0d
nico778 <n.mogicato@gmail.com>
2022-05-10 17:26:20 +0200

    revised buildPM()

--- src/geometry/pm.ts
+++ src/geometry/pm.ts
@@ -11,7 +11,8 @@ export class PMesh {
   edges: Edge[];
   faces: Face[];
   halfedges: Halfedge[];
-
+  
+	boundary: Face[]
   vsplits: Vsplit[];
   full_nvertices: number; //number of vertices in Mn
   full_nwedges: number; //number of wedges in Mn
@@ -27,12 +28,14 @@ export class PMesh {
   anc_wattribs?: Wedge[];
   
   constructor(data: String) {
-    const indices: number[] = [];
     const positions: Vector[] = [];
+    const indices: number[] = [];
+    
     this.verts = [];
     this.edges = [];
     this.faces = [];
     this.halfedges = [];
+    this.boundary = [];
     this.vsplits = [];
     this.full_nvertices = 0; //number of vertices in Mn
     this.full_nwedges = 0; //number of wedges in Mn
@@ -60,145 +63,202 @@ export class PMesh {
   }
 
   buildPM(indices: number[], positions: Vector[]) {
-    this.verts = new Array(positions.length);
-    this.edges = new Array(positions.length + indices.length/3);
-    this.faces = new Array(indices.length / 3);
-    this.halfedges = new Array((positions.length + indices.length/3) * 2);
+    let vertexMap = new Map();
+    //for finding out the number of edges initially before creating them
+		let findEdges = new Map();
+		//for finding out the number of boundary halfedges initially before creating them
+    let findBHalfedges = 0;
+    //find edges by looping through faces(indices technically)
+    //fst == first vertex, scnd == second vertex
+		for(let i = 0; i < indices.length; i += 3) {
+			for(let j = 0; j < 3; j++) {
+				let fst;
+				let scnd;
+        //assign correct vertices, depending on which side of face
+				if(j === 0 || j === 1) {
+					fst = indices[i + j];
+					scnd = indices[i + j +1];
+				} else {
+					fst = indices[i + j];
+					scnd = indices[i];
+				}
+				//for easier search in map, switch if a has higher index than b
+				if(fst > scnd) {
+					fst = fst ^ scnd;
+					scnd = fst ^ scnd;
+					fst = fst ^ scnd;
+				}
+        //generate key as string
+				let fe = String([fst, scnd]);
+        //add future edge if not found in map and treat as future boundary halfedge
+        //number of future boundary halfedges automatically corrected
+				if(findEdges.has(fe)) {
+					findBHalfedges--;
+				} else {
+					findEdges.set(fe, [fst, scnd]);
+					findBHalfedges++;
+				}
+			}
+		}
 
-    //create vertices
-    for (let i = 0; i < positions.length; i++) {
-      const v = new Vertex(positions[i]);
-      this.verts[i] = v;
-    }
+		//allocate necessary space for individual elements of mesh
+		this.verts = new Array(positions.length);
+		this.faces = new Array(indices.length / 3);
+		this.halfedges = new Array(2 * findEdges.size);
+		this.edges = new Array(findEdges.size);
 
-    let eidx = 0;
-    const existedHe = new Map();
-    const hasTwin = new Map();
+		//create vertices and store vertex + index in map 
+		for(let i = 0; i < positions.length; i++) {
+			let v = new Vertex(positions[i]);
+			vertexMap.set(i, v);
+			this.verts[i] = v;
+		}
 
-    // construct halfedges, edges
-    for (let i = 0; i < indices.length; i += 3) {
-      // construct face
-      const f = new Face();
-      this.faces[i / 3] = f;
+		//loop through all indices(future faces) and create faces, halfedges and lastly edges
+    //leave all boundary stuff for later
+		let createdHalfedges = new Map();
+		let twinHalfedges = new Map();
+		let edgeIndex = 0;
+		for(let i = 0; i < indices.length; i += 3) {
+			//create new face
+			let f = new Face();
+			this.faces[i / 3] = f;
+			//create the 3 halfedges of our new face
+			for(let j = 0; j < 3; j++) {
+				let h = new Halfedge();
+				this.halfedges[i + j] = h;
+			}
+			//add halfedge connectivity to our 3 new halfedges
+			for(let j = 0; j < 3; j++) {
+				let fst;
+				let scnd;
+				//fst == first vertex, scnd == second vertex
+        //assign correct index depending on halfedge
+				if(j === 0 || j === 1) {
+					fst = indices[i + j];
+					scnd = indices[i + j +1];
+				} else {
+					fst = indices[i + j];
+					scnd = indices[i];
+				}
 
-      // construct halfedges of the face
-      for (let j = 0; j < 3; j++) {
-        const he = new Halfedge();
-        this.halfedges[i + j] = he;
-      }
+				let h = this.halfedges[i + j];
+        //link to correct next and previous halfedge
+				if(j === 0 || j === 1) {
+					h.next = this.halfedges[i + j + 1];
+				} else {
+					h.next = this.halfedges[i];
+				}
+				if(j === 0) {
+					h.prev = this.halfedges[i + 2];
+				} else {
+					h.prev = this.halfedges[i + j - 1];
+				}
 
-      // construct connectivities of the new halfedges
-      for (let j = 0; j < 3; j++) {
-        // halfedge from vertex a to vertex b
-        let a = indices[i + j];
-        let b = indices[i + ((j + 1) % 3)];
+        //per default onBoundary is false, same for twin halfedge map
+				twinHalfedges.set(h, false);
+				h.onBoundary = false;
+				//link new halfedges and vertices by using previously created vertexMap
+				//link new halfedges and face
+				let v = vertexMap.get(fst);
+				v.halfedge = h;
+				h.vert = v;
+				f.halfedge = h;
+				h.face = f;
 
-        // halfedge properties
-        const he = this.halfedges[i + j];
-        he.next = this.halfedges[i + ((j + 1) % 3)];
-        he.prev = this.halfedges[i + ((j + 2) % 3)];
-        he.onBoundary = false;
-        hasTwin.set(he, false);
+				//for easier search in map, switch if a has higher index than b
+				if(fst > scnd) {
+					fst = fst ^ scnd;
+					scnd = fst ^ scnd;
+					fst = fst ^ scnd;
+				}
 
-        //const v = idx2vert.get(a);
-        const v = this.verts[a];
-        he.vert = v;
-        v.halfedge = he;
+        //store
+				let cH = String([fst, scnd]);
+        //if an halfedge between current two vertices already exists
+        //we add both to the twin map
+				if(createdHalfedges.has(cH)) {
+          let twin = createdHalfedges.get(cH);
+					twinHalfedges.set(h, true);
+					twinHalfedges.set(twin, true);
+          //finish connectivity by linking twins together + edge
+					h.twin = twin;
+					h.edge = twin.edge;
+					twin.twin = h;
+				} else {
+					//if such an halfedge does not exist we create its edge and
+          //we add halfedge to existingHalfedges map
+					let e = new Edge();
+					this.edges[edgeIndex++] = e;
+          createdHalfedges.set(cH, h);
+          //link edge and halfedge
+					h.edge = e;
+					e.halfedge = h;
+				}
+			}
+		}
 
-        he.face = f;
-        f.halfedge = he;
+		//create second half of boundary halfedges and virtual faces(within boundary cycle)
+		//also create and insert corners
+		let append = indices.length;
+		//loop over previously created halfedges(by looping over all indices)
+		for(let i = 0; i < indices.length; i++) {
+			let h = this.halfedges[i];
+			//if halfedge has no twin it has to be on the boundary
+			if(!twinHalfedges.get(h)) {
+				//create virtual boundary face and add to boundary array
+				let f = new Face();
+				this.boundary.push(f);
 
-        // swap if index a > b, for twin checking
-        if (a > b) {
-          const tmp = b;
-          b = a;
-          a = tmp;
-        }
-        const ek = `${a}-${b}`;
-        if (existedHe.has(ek)) {
-          // if a halfedge has been created before, then
-          // it is the twin halfedge of the current halfedge
-          const twin = existedHe.get(ek);
-          he.twin = twin;
-          twin.twin = he;
-          he.edge = twin.edge;
+				//boundary round
+				let addition = h;
+				let counter = 0;
+				let boundaryRound = [];
+				//we already determined the number of future boundary halfedges in the beginning
+				while(counter <= findBHalfedges) {
+					//create the new boundary halfedge
+					let boundaryH = new Halfedge();
+					append++;
+					//add it to the boundary round and the halfedges array of the mesh
+					boundaryRound.push(boundaryH);
+					this.halfedges[append] = boundaryH;
 
-          hasTwin.set(he, true);
-          hasTwin.set(twin, true);
-        } else {
-          // new halfedge
-          const e = new Edge();
-          this.edges[eidx] = e;
-          eidx++;
-          he.edge = e;
-          e.halfedge = he;
+					//now we need to find the next halfedge on the boundary without a twin
+					let nextTwinless = addition.next;
+					while(twinHalfedges.get(nextTwinless)) {
+						nextTwinless = nextTwinless!.twin!.next;
+					}
 
-          existedHe.set(ek, he);
-        }
+					//link twins
+					//link new halfedge and new face
+					//complete the rest of the halfedge properties
+					boundaryH.twin = addition;
+					addition.twin = boundaryH;
+					boundaryH.face = f;
+					f.halfedge = boundaryH;
+					boundaryH.vert = nextTwinless!.vert;
+					boundaryH.edge = addition.edge;
+					boundaryH.onBoundary = true;
 
-        // FIXME: non-manifold edge count checking
-      }
-    }
+					//next step of boundary round
+					addition = nextTwinless!;
+					counter++;
+				};
+				console.log(counter);
+				//link all existing boundary halfedges in correct order
+				for(let j = 0; j < counter; j++) {
+					boundaryRound[j].prev = boundaryRound[(j + 1) % counter];
+					boundaryRound[j].next = boundaryRound[(counter + j -1) % counter];
+					twinHalfedges.set(boundaryRound[j], true);
+					twinHalfedges.set(boundaryRound[j].twin, true);
+				}
+			}
+		}
 
-    // create boundary halfedges and hidden faces for the boundary
-    let hidx = indices.length;
-    for (let i = 0; i < indices.length; i++) {
-      const he = this.halfedges[i];
-      if (hasTwin.get(he)) {
-        continue;
-      }
-
-      // handle halfedge that has no twin
-      const f = new Face(); // hidden face
-      const bcycle = []; // boundary cycle
-      let current = he;
-      do {
-        const bhe = new Halfedge(); // boundary halfedge
-        this.halfedges[hidx] = bhe;
-        hidx++;
-        bcycle.push(bhe);
-
-        // grab the next halfedge along the boundary that does not
-        // have a twin halfedge
-        let next = <Halfedge>current.next;
-        while (hasTwin.get(next)) {
-          next = <Halfedge>next.twin!.next;
-        }
-
-        // set the current halfedge's attributes
-        bhe.vert = next.vert;
-        bhe.edge = current.edge;
-        bhe.onBoundary = true;
-
-        // point the new halfedge and face to each other
-        bhe.face = f;
-        f.halfedge = bhe;
-
-        // point the new halfedge and twin to each other
-        bhe.twin = current;
-        current.twin = bhe;
-
-        current = next;
-      } while (current !== he);
-
-      // link the cycle of boundary halfedges together
-      const n = bcycle.length;
-      for (let j = 0; j < n; j++) {
-        bcycle[j].next = bcycle[(j + n - 1) % n];
-        bcycle[j].prev = bcycle[(j + 1) % n];
-        hasTwin.set(bcycle[j], true);
-        hasTwin.set(bcycle[j].twin, true);
-      }
-    }
-
-    // reset indices
+    //update correct indices
     let index = 0;
     this.verts.forEach(v => {
       v.idx = index++;
-    });
-    index = 0;
-    this.edges.forEach(e => {
-      e.idx = index++;
     });
     index = 0;
     this.faces.forEach(f => {
@@ -208,11 +268,15 @@ export class PMesh {
     this.halfedges.forEach(h => {
       h.idx = index++;
     });
+		index = 0;
+    this.edges.forEach(e => {
+      e.idx = index++;
+    });
 
-    console.log(this.verts.length)
-    console.log(this.faces.length)
-    console.log(this.halfedges.length)
-    console.log(this.edges.length)
+    console.log(this.verts.length);
+    console.log(this.faces.length);
+    console.log(this.halfedges.length);
+    console.log(this.edges.length);
   }
 
   //apply vsplit
@@ -222,7 +286,7 @@ export class PMesh {
   //apply ecol
   pmIteratorPrev() {
     return new Vector();
-  }  
+  }
   //go to specified # of vertices/faces
   pmIteratorgoto(goal_nvertices: number, goal_nfaces: number) {
     return new Vector();
