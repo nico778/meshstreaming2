b9db3580f6f21fc4cffb240df06e21bb3dbe18cd
nico778 <n.mogicato@gmail.com>
2022-05-15 14:21:21 +0200

    remove build files

--- dist/server/geometry/edge.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Edge = void 0;
-class Edge {
-    constructor() {
-        this.idx = -1;
-    }
-}
-exports.Edge = Edge;
