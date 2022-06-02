1895f8fe093342c71fc003b9ee22b7d019b87254
nico778 <n.mogicato@gmail.com>
2022-06-02 12:35:29 +0200

    cleanup

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,11 +3,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GUI } from 'dat.gui'
 import { io } from 'socket.io-client'
 
-const vertices = new Float32Array( 32000 * 3 ); 
-//const indices = new Float32Array( 20 * 3 ); 
-//let vertices: number[];
-//vertices = [];
-//const indices: number[];
+const vertices = new Float32Array(32000 * 3 );
 const indices = new Array(100000 * 3);
 let updates: number[];
 updates = [];
@@ -32,219 +28,139 @@ let geometry = new THREE.BufferGeometry();
 
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    render();
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	render();
 }
 
 function buildMesh() {
-    //console.log(indices)
-    //var positions = new Float32Array( 99 ); // 3 vertices per point
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-    //geometry.setDrawRange(0, 27);
-    //geometry.setAttribute('indices', new THREE.Float32BufferAttribute(indices, 3));
-    geometry.setIndex(indices)
-    const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-    animate();
+  geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+  geometry.setIndex(indices)
+	const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+	mesh = new THREE.Mesh(geometry, material);
+	scene.add(mesh);
+	animate();
 }
 
 const socket = io();
 socket.on('connect', function () {
-    console.log('connect');
+	console.log('connect');
 });
 socket.on('disconnect', function (message: any) {
-    console.log('disconnect ' + message);
+	console.log('disconnect ' + message);
 });
 
 socket.on('stream vertices', (verts:number[]) => {
-    //for(let i=0; i < 27; i++) {
-      //  vertices[i] = verts[i];
-    //}
+	initialvertices = verts.length;
 
-    initialvertices = verts.length;
-
-    let i=0
-    verts.forEach( v => {
-        vertices[i] = v;
-        i++;
-    });
-    //console.log(vertices)
+	let i=0
+	verts.forEach( v => {
+		vertices[i] = v;
+		i++;
+	});
 });
 socket.on('stream indices', (inds:number[]) => {
-    //inds.forEach( i => {
-      //  indices.push(i);
-    //});
-    
-    
-    inds.forEach( f => {
-        indices[addPoint] = f;
-        addPoint++;
-    });
-    //console.log(indices)
-    buildMesh();
+	inds.forEach( f => {
+		indices[addPoint] = f;
+		addPoint++;
+	});
+	buildMesh();
 });
  
 socket.on('update vertices', (vertices) => {
-    //geometry.attributes.position.needsUpdate = true;
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-    //console.log(vertices)
+  geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
 });
 socket.on('update indices', (indices) => {
-    geometry.setIndex(indices);
-    //console.log(indices)
+  geometry.setIndex(indices);
 });
 
 socket.on('stream updates', (updates2) => {
-    geometry.setIndex(indices);
-    //console.log(indices)
+  geometry.setIndex(indices);
 });
 
 socket.on('stream vsplit', (updates2, vertices2, indices2) => {
-    for(let i = 0; i < indices.length; i++) {
-        indices[i]++;
-    }
+	for(let i = 0; i < indices.length; i++) {
+		indices[i]++;
+	}
 
-    for(let i = 0; i < indices.length; i++) {
-        if(indices[i] > vertices2[0]) {
-            indices[i]++;
-        }
-    }
+	for(let i = 0; i < indices.length; i++) {
+		if(indices[i] > vertices2[0]) {
+			indices[i]++;
+		}
+	}
 
-    //update indices of faces with vertex change not part of vsplit
-    for(let i = 0; i < indices.length; i+=3) {
-        for(let j = 0; j < updates2.length; j+=3) {
-            let control = [updates2[j], updates2[j+1], updates2[j+2]]
-            check = [indices[i], indices[i+1], indices[i+2]]
-            //console.log(control)
-            //console.log(check)
+	//update indices of faces with vertex change not part of vsplit
+	for(let i = 0; i < indices.length; i+=3) {
+		for(let j = 0; j < updates2.length; j+=3) {
+			let control = [updates2[j], updates2[j+1], updates2[j+2]]
+			check = [indices[i], indices[i+1], indices[i+2]]
 
-            if(control.every(r => check.includes(r))){
-                console.log('Found all of', control, 'in', check);
-                //console.log(indices[i],indices[i+1],indices[i+2])
-                if(indices[i] === (vertices2[1] + 1)) {
-                    console.log('0')
-                    indices[i] = vertices2[0] + 1
-                } else if(indices[i+1] === (vertices2[1] + 1)) {
-                    console.log('1')
-                    indices[i+1] = vertices2[0] + 1
-                } else if(indices[i+2] === (vertices2[1] + 1)) {
-                    console.log('2')
-                    indices[i+2] = vertices2[0] + 1
-                }
-            }
-        }
-    }
+			if(control.every(r => check.includes(r))){
+				console.log('Found', control, 'in', check);
+				if(indices[i] === (vertices2[1] + 1)) {
+					console.log('0')
+					indices[i] = vertices2[0] + 1
+				} else if(indices[i+1] === (vertices2[1] + 1)) {
+					console.log('1')
+					indices[i+1] = vertices2[0] + 1
+				} else if(indices[i+2] === (vertices2[1] + 1)) {
+					console.log('2')
+					indices[i+2] = vertices2[0] + 1
+				}
+			}
+		}
+	}
 
-    check = []
+	check = []
 
-    //update vertices
-    vertices.copyWithin(vertices2[0]*3 + 3, vertices2[0]*3);
+	//update vertices
+	vertices.copyWithin(vertices2[0]*3 + 3, vertices2[0]*3);
 
-    //vertices.copyWithin(vertices2[0]*3 + 3, vertices2[0]*3);
+	let newVert = vertices2[0]*3;
 
-    let newVert = vertices2[0]*3;
+	vertices[newVert] = vertices2[2]
+	vertices[newVert + 1] = vertices2[3]
+	vertices[newVert + 2] = vertices2[4]
 
-    vertices[newVert] = vertices2[2]
-    vertices[newVert + 1] = vertices2[3]
-    vertices[newVert + 2] = vertices2[4]
+	//add new faces
+	for(let i = 0; i < 6; i++) {
+		indices[addPoint] = indices2[i];
+		addPoint++;
+	}
 
-    //add new faces
-    for(let i = 0; i < 6; i++) {
-        indices[addPoint] = indices2[i];
-        addPoint++;
-    }
+	console.log(vertices)
+	console.log(indices)
+	console.log(updates2)
+	console.log(vertices2)
+	console.log(indices2)
 
-    console.log(vertices)
-    console.log(indices)
-    console.log(updates2)
-    console.log(vertices2)
-    console.log(indices2)
+	for(let i = 0; i < indices.length; i++) {
+		indices[i]--;
+	}
 
-    for(let i = 0; i < indices.length; i++) {
-        indices[i]--;
-    }
-
-    buildMesh()
-/*
-    
-
-    //update indices
-    for(let i = 0; i < indices.length; i+=3) { 
-        for(let j = 0; j < updates2.length; j+=3) {
-            let skip = [updates2[j], updates2[j+1], updates2[j+2]]
-            let skipface = [indices[i], indices[i+1], indices[i+2]]
-            if(skip.every(r => skipface.includes(r))) {
-                console.log('skip')
-                break;
-            }
-        if(i === 9 || i === 27) {
-            console.log('skip')
-            continue;
-        } 
-
-        if(indices[i] >= vertices2[0]) {
-            let update = indices[i];
-            indices[i] = update + 1;
-        } if(indices[i+1] >= vertices2[0]) {
-            let update = indices[i+1];
-            indices[i+1] = update + 1;
-        } if(indices[i+2] >= vertices2[0]) {
-            let update = indices[i+2];
-            indices[i+2] = update + 1;
-        }
-    //}
-    }
-
-    vertices.copyWithin(vertices2[0]*3 + 3, vertices2[0]*3);
-
-    //vertices.copyWithin(vertices2[0]*3 + 3, vertices2[0]*3);
-
-    let newVert = vertices2[0]*3;
-
-    vertices[newVert] = vertices2[2]
-    vertices[newVert + 1] = vertices2[3]
-    vertices[newVert + 2] = vertices2[4]
-
-
-    //if(vertices2[0] !== 0) {
-    //}
-    
-
-    for(let i = 0; i < 6; i++) {
-        indices[addPoint] = indices2[i] - 1;
-        addPoint++;
-    }
-
-
-    //console.log(vertices)
-    console.log(indices)
-
-    buildMesh();
-*/
+	buildMesh()
 });
 
 function startStreaming() {
-    var type = params.type;
-    //console.log(type);
-    /*if (mesh !== null) {
-        scene.remove(mesh);
-        vertices = [];
-        indices = [];
-      }*/
-    socket.emit('request mesh', type);
+	var type = params.type;
+
+	/*if (mesh !== null) {
+			scene.remove(mesh);
+			vertices = [];
+			indices = [];
+		}*/
+	socket.emit('request mesh', type);
 }
 
 function startCollapsing() {
-    socket.emit('request simplify');
+	socket.emit('request simplify');
 }
 
 function shiftRight(array: Float32Array) {
-    array.set(array.subarray(0, -3), 3)
-    array.fill(0, 0, 3)
-    return array
+	array.set(array.subarray(0, -3), 3)
+	array.fill(0, 0, 3)
+	return array
 }
 
 
@@ -256,39 +172,39 @@ const modelsFolder = gui.addFolder('Select Model');
 
 //add button to start mesh streaming
 var params = { 
-    stream: () => startStreaming(),
-    simplify: () => startCollapsing(),
-    type: ['cube', 'icosahedron', 'gourd', 'monkey']
+	stream: () => startStreaming(),
+	simplify: () => startCollapsing(),
+	type: ['cube', 'icosahedron', 'gourd', 'monkey']
 };
 
 gui
-    .add(params,'stream')
-    .name('stream mesh from server')
-    .listen();
+	.add(params,'stream')
+	.name('stream mesh from server')
+	.listen();
 
 gui
-    .add(params,'simplify')
-    .name('simplify mesh')
-    .listen();
+	.add(params,'simplify')
+	.name('simplify mesh')
+	.listen();
 
 gui
-    .add(params, 'type', [
-        'cube',
-        'icosahedron',
-        'gourd',
-        'monkey'
-    ])
-    .name('select model')
-    .listen();
+	.add(params, 'type', [
+		'cube',
+		'icosahedron',
+		'gourd',
+		'monkey'
+	])
+	.name('select model')
+	.listen();
 
 const animate = function () {
-    requestAnimationFrame(animate);
-    controls.update();
-    render();
+	requestAnimationFrame(animate);
+	controls.update();
+	render();
 }
 
 const render = function () {
-    renderer.render(scene, camera);
+	renderer.render(scene, camera);
 }
 
 animate();
--- src/server/server.ts
+++ src/server/server.ts
@@ -10,7 +10,6 @@ import * as readline from 'readline';
 
 const port: number = 3000;
 
-//const vertices = new Float32Array( 200 )
 let vertices: number[];
 let indices: number[];
 let updates2: number[];
@@ -26,211 +25,157 @@ indices2 = [];
 let pmesh: PMesh;
 
 class App {
-    private server: http.Server;
-    private port: number;
+	private server: http.Server;
+	private port: number;
 
-    private io: Server;
-    private clients: any = {};
+	private io: Server;
+	private clients: any = {};
 
-    constructor(port: number) {
-        this.port = port;
-        const app = express();
-        app.use(express.static(path.join(__dirname, '../client')));
+	constructor(port: number) {
+		this.port = port;
+		const app = express();
+		app.use(express.static(path.join(__dirname, '../client')));
 
-        this.server = new http.Server(app);
+		this.server = new http.Server(app);
 
-        this.io = new Server(this.server);
+		this.io = new Server(this.server);
 
-        this.io.on('connection', (socket: Socket) => {
-            console.log(socket.constructor.name);
-            this.clients[socket.id] = {};
-            console.log(this.clients);
-            console.log('a user connected : ' + socket.id);
+		this.io.on('connection', (socket: Socket) => {
+			console.log(socket.constructor.name);
+			this.clients[socket.id] = {};
+			console.log(this.clients);
+			console.log('a user connected : ' + socket.id);
 
-            //socket.on('request mesh', (msg: any) => {
-            (async function processLineByLine() {
-              try {
-                const rl = readline.createInterface({
-                  input: fs.createReadStream('base-mesh2.txt'),
-                  crlfDelay: Infinity
-                });
-                let vidx = 0;
-                rl.on('line', (line) => {
-                  console.log(`Line from file: ${line}`);
+			(async function processLineByLine() {
+				try {
+					const rl = readline.createInterface({
+						input: fs.createReadStream('base-mesh2.txt'),
+						crlfDelay: Infinity
+					});
+					let vidx = 0;
+					rl.on('line', (line) => {
+						console.log(`Line from file: ${line}`);
 
-                  let chunks = line.split(' ');
-                  switch(chunks[0]) {
-                    case 'v':
-                      //vertices[vidx] = parseFloat(chunks[1])
-                      //vertices[vidx++] = parseFloat(chunks[2])
-                      //vertices[vidx++] = parseFloat(chunks[3])
+						let chunks = line.split(' ');
+						switch(chunks[0]) {
+							case 'v':
+								vertices.push(parseFloat(chunks[1]));
+								vertices.push(parseFloat(chunks[2]));
+								vertices.push(parseFloat(chunks[3]));
+								break;
+							case 'f':
+								for(let i=1; i < 4; i++) {
+									let index = chunks[i].split(' ');
+									indices.push(parseInt(index[0]) - 1);
+								}
+								break;
+							case '-':
+								socket.emit('stream vertices', vertices);
+								socket.emit('stream indices', indices);
+								break;
+							case 'u':
+								for(let i=1; i < 4; i++) {
+									let index = chunks[i].split(' ');
+									updates2.push(parseInt(index[0]));
+								}
+								console.log(chunks[1])
+								console.log(chunks[2])
+								console.log(chunks[3])
+								console.log(chunks[4])
+								break;
+							case 'x':
+								vertices2.push(parseFloat(chunks[1]));
+								vertices2.push(parseFloat(chunks[2]));
+								vertices2.push(parseFloat(chunks[3]));
+								vertices2.push(parseFloat(chunks[4]));
+								vertices2.push(parseFloat(chunks[5]));
+								break;
+							case 'y':
+								for(let i=1; i < 4; i++) {
+									let index = chunks[i].split(' ');
+									indices2.push(parseInt(index[0]));
+								}
+								console.log(chunks[1])
+								console.log(chunks[2])
+								console.log(chunks[3])
+								console.log(chunks[4])
+								break;
+							case 'z':
+								for(let i=1; i < 5; i++) {
+									let index = chunks[i].split(' ');
+									indices2.push(parseInt(index[0]));
+								}
+								console.log(chunks[1])
+								console.log(chunks[2])
+								console.log(chunks[3])
+								console.log(chunks[4])
+								socket.emit('stream vsplit', updates2, vertices2, indices2);
+								vertices2 = [];
+								indices2 = [];
+								updates2 = [];
+								break;
+						}
+					});
+					
+					await events.EventEmitter.once(rl, 'close');
+				} catch (err) {
+					console.error(err);
+				}
+			})();
 
-                      vertices.push(parseFloat(chunks[1]));
-                      vertices.push(parseFloat(chunks[2]));
-                      vertices.push(parseFloat(chunks[3]));
-                      break;
-                    case 'f':
-                      for(let i=1; i < 4; i++) {
-                        let index = chunks[i].split(' ');
-                        indices.push(parseInt(index[0]) - 1);
-                      }
-                      break;
-                    case '-':
-                      socket.emit('stream vertices', vertices);
-                      socket.emit('stream indices', indices);
-                      break;
-                    case 'u':
-                      for(let i=1; i < 4; i++) {
-                        let index = chunks[i].split(' ');
-                        updates2.push(parseInt(index[0]));
-                      }
-                      console.log(chunks[1])
-                      console.log(chunks[2])
-                      console.log(chunks[3])
-                      console.log(chunks[4])
-                      break;
-                    case 'x':
-                      vertices2.push(parseFloat(chunks[1]));
-                      vertices2.push(parseFloat(chunks[2]));
-                      vertices2.push(parseFloat(chunks[3]));
-                      vertices2.push(parseFloat(chunks[4]));
-                      vertices2.push(parseFloat(chunks[5]));
-                      break;
-                    case 'y':
-                      for(let i=1; i < 4; i++) {
-                        let index = chunks[i].split(' ');
-                        indices2.push(parseInt(index[0]));
-                      }
-                      console.log(chunks[1])
-                      console.log(chunks[2])
-                      console.log(chunks[3])
-                      console.log(chunks[4])
-                      break;
-                    case 'z':
-                      for(let i=1; i < 5; i++) {
-                        let index = chunks[i].split(' ');
-                        indices2.push(parseInt(index[0]));
-                      }
-                      console.log(chunks[1])
-                      console.log(chunks[2])
-                      console.log(chunks[3])
-                      console.log(chunks[4])
-                      socket.emit('stream vsplit', updates2, vertices2, indices2);
-                      vertices2 = [];
-                      indices2 = [];
-                      updates2 = [];
-                      break;
-                  }
-                });
-                
-                await events.EventEmitter.once(rl, 'close');
-                
-                //console.log('Reading file line by line with readline done.');
-                //const used = process.memoryUsage().heapUsed / 1024 / 1024;
-                //console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
-              } catch (err) {
-                console.error(err);
-              }
-            })();
+			socket.on('request mesh', (msg: any) => {
+				var path = './src/models/'+msg+'.obj';
+				fs.readFile(path, 'utf-8', (err: any, data: String) => {
+					if(err) {
+						console.error(err);
+						return;
+					}
+					vertices = [];
+					indices = [];
 
-/*
-                var path = 'base-mesh.txt';
-                fs.readFile(path, 'utf-8', (err: any, data: String) => {
-                  if(err) {
-                    console.error(err);
-                    return;
-                  }
+					pmesh = new PMesh(data);
+					pmesh.verts.forEach(v => {
+						vertices.push(v.position.x);
+						vertices.push(v.position.y);
+						vertices.push(v.position.z);
+					});
+					pmesh.faces.forEach(f => {
+						indices.push(f.halfedge!.vert!.idx);
+						indices.push(f.halfedge!.next!.vert!.idx);
+						indices.push(f.halfedge!.next!.next!.vert!.idx);
+					});
+					socket.emit('stream vertices', vertices);
+					socket.emit('stream indices', indices);
+				});
+			});
 
-                  pmesh = new PMesh(data);
-                  pmesh.verts.forEach(v => {
-                      vertices.push(v.position.x);
-                      vertices.push(v.position.y);
-                      vertices.push(v.position.z);
-                  });
-                  pmesh.faces.forEach(f => {
-                      indices.push(f.halfedge!.vert!.idx);
-                      indices.push(f.halfedge!.next!.vert!.idx);
-                      indices.push(f.halfedge!.next!.next!.vert!.idx);
-                  });
-                  socket.emit('stream vertices', vertices);
-                  socket.emit('stream indices', indices);
-                });*/
-            //});
+			socket.on('request simplify', () => {
+				pmesh.pm_simplify();
 
-            socket.on('request mesh', (msg: any) => {
-                var path = './src/models/'+msg+'.obj';
-                fs.readFile(path, 'utf-8', (err: any, data: String) => {
-                  if(err) {
-                    console.error(err);
-                    return;
-                  }
-                  vertices = [];
-                  indices = [];
+				vertices = [];
+				indices = [];                                                                                  
 
-                  pmesh = new PMesh(data);
-                  pmesh.verts.forEach(v => {
-                      vertices.push(v.position.x);
-                      vertices.push(v.position.y);
-                      vertices.push(v.position.z);
-                  });
-                  pmesh.faces.forEach(f => {
-                      indices.push(f.halfedge!.vert!.idx);
-                      indices.push(f.halfedge!.next!.vert!.idx);
-                      indices.push(f.halfedge!.next!.next!.vert!.idx);
-                  });
-                  socket.emit('stream vertices', vertices);
-                  socket.emit('stream indices', indices);
-                });
-            });
+				pmesh.verts.forEach(v => {
+					vertices.push(v.position.x);
+					vertices.push(v.position.y);
+					vertices.push(v.position.z);
+				});
+				pmesh.faces.forEach(f => {
+					indices.push(f.halfedge!.vert!.idx);
+					indices.push(f.halfedge!.next!.vert!.idx);
+					indices.push(f.halfedge!.next!.next!.vert!.idx);
+				});
+				socket.emit('update vertices', vertices);
+				socket.emit('update indices', indices);
+			});
+		});
+	}
 
-            socket.on('request simplify', () => {
-                pmesh.pm_simplify();
-
-                vertices = [];
-                indices = [];                                                                                  
-
-                pmesh.verts.forEach(v => {
-                    vertices.push(v.position.x);
-                    vertices.push(v.position.y);
-                    vertices.push(v.position.z);
-                });
-                pmesh.faces.forEach(f => {
-                    indices.push(f.halfedge!.vert!.idx);
-                    indices.push(f.halfedge!.next!.vert!.idx);
-                    indices.push(f.halfedge!.next!.next!.vert!.idx);
-                });
-                socket.emit('update vertices', vertices);
-                socket.emit('update indices', indices);
-            });
-        });
-    }
-
-    public Start() {
-        this.server.listen(this.port, () => {
-            console.log(`Server listening on port ${this.port}.`);
-        });
-    }
-
-    /*parseFile() {
-        const obj = data.split(/\r?\n/);
-        obj.forEach( line => {
-          let chunks = line.split(' ');
-          switch(chunks[0]) {
-            case 'v':
-              vertices2.push(
-                new Vector(parseFloat(chunks[1]), parseFloat(chunks[2]),
-                parseFloat(chunks[3]), 1)
-              );
-              break;
-            case 'f':
-              for(let i=1; i < 4; i++) {
-                let index = chunks[i].split(' ');
-                indices2.push(parseInt(index[0]) - 1);
-              }
-              break;
-          }
-        });
-    }*/
+	public Start() {
+		this.server.listen(this.port, () => {
+			console.log(`Server listening on port ${this.port}.`);
+		});
+	}
 }
 
 new App(port).Start()
